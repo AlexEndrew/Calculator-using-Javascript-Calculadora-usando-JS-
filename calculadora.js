a35986ce5e481e2cc4ea0444584fd35a01b9fd56
Alex Endrew <105590668+AlexEndrew@users.noreply.github.com>
2022-07-02 12:10:21 -0300

    Add files via upload

--- calculadora.js
+++ calculadora.js
@@ -8,26 +8,45 @@ function calculadora() {
     function soma(){
         resultado = n1+n2;
         alert(`${n1} + ${n2} = ${resultado}`);
+        novaOperacao();
     }
     function subtracao(){
         resultado = n1-n2;
         alert(`${n1} - ${n2} = ${resultado}`);
+        novaOperacao();
     }
     function multiplicacao(){
         resultado = n1*n2;
         alert(`${n1} * ${n2} = ${resultado}`);
+        novaOperacao();
     }
     function divisaoReal(){
         resultado = n1/n2;
         alert(`${n1} / ${n2} = ${resultado}`);
+        novaOperacao();
     }
     function divisaoInteira(){
         resultado = n1%n2;
         alert(`${n1} % ${n2} = ${resultado}`);
+        novaOperacao();
     }
     function potenciacao(){
         resultado = n1**n2;
         alert(`${n1} ** ${n2} = ${resultado}`);
+        novaOperacao();
+    }
+
+    function novaOperacao(){
+        let opcao = prompt('Deseja fazer outra operação?\n 1-Sim\n2-Não');
+
+        if (opcao == 1) {
+            calculadora();
+        } else if (opcao == 2){
+            alert('Até mais!');
+        } else {
+            alert('Digite uma opção válida!');
+            novaOperacao();
+        }
     }
 
     if (operacao == 1) {
